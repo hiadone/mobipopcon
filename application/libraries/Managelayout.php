0e2dd31f905d5caa0c9f3ae5e3d062cc4f0f36c9
boyoon <boyoon8@gmai.com>
2017-03-06 10:04:16 +0900

    swipe 초기

--- application/libraries/Managelayout.php
+++ application/libraries/Managelayout.php
@@ -254,6 +254,10 @@ class Managelayout extends CI_Controller
             $CI->load->library('popuplib');
             $data['popup'] = $CI->popuplib->display_popup();
 
+        } else {
+            // 메뉴관리
+            $CI->load->model('Menu_model');
+            $data['menu'] = $CI->Menu_model->get_all_menu($device_view_type);
         }
 
         return $data;
